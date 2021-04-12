d2ea282f93de68eead48a2abe75d30d4f3d162d6
Anduin9527 <anduin9527@gmail.com>
2021-04-12 16:37:20 +0800

    main

--- inc/Date.h
+++ inc/Date.h
@@ -1,20 +1,16 @@
 #include <iostream>
 
-
-
 class Date
 {
 private:
-    int year;
-    int month;
-    int day;
-    int hour;
-    int minute;
+  int year;
+  int month;
+  int day;
+  int hour;
+  int minute;
 
 public:
-    Date(int a = 0, int b = 0, int c = 0,int d=0, int e=0);
-    ~Date();
-    void getDate();
+  Date(int a = 0, int b = 0, int c = 0, int d = 0, int e = 0);
+  ~Date();
+  void getDate();
 };
-
-
