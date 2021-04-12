5070eac0e9d4263fb43934f64c664977ba8a0649
Anduin9527 <anduin9527@gmail.com>
2021-04-12 16:49:06 +0800

    main

---
+++ Flight.cpp
@@ -0,0 +1,21 @@
+#include "inc/Flight.h"
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+void Flight::Add(string FNumber, string Airways,
+         string startPoint, string endPoint,
+         Date PlanDepartureTime, Date PlanArrivalTime,
+         int Price, int Tickits)
+{
+  this->FNumber = FNumber;
+  this->Airways = Airways;
+  this->startPoint = startPoint;
+  this->endPoint = endPoint;
+  this->PlanDepartureTime = PlanDepartureTime;
+  this->
+  this->Price = Price;
+
+}
