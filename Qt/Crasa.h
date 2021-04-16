27d46a72d5789134ff50072aba9f936d5e03423c
Anduin9527 <anduin9527@gmail.com>
2021-04-16 10:01:54 +0800

    main

--- Qt/Crasa.h
+++ Qt/Crasa.h
@@ -22,76 +22,50 @@ bool isRegist(string &ID)
   }
   return false;
 }
-void Encode(string &ID, string &str, int k = 5) //加密
+string Encode(string &str, int k = 5) //加密
 {
-  ofstream File("Account.dat",
-                std::ios::out | std::ios::app);
-  File << ID << endl;
   for (unsigned int i = 0; i < str.length(); i++)
   {
     if (97 <= str[i] && str[i] <= 122) //小写
-    {
       str[i] = ((str[i] + k - 97) % 26) + 97;
-      File << str[i];
-    }
+
     else if (65 <= str[i] && str[i] <= 90) //大写
-    {
       str[i] = ((str[i] + k - 65) % 26) + 65;
-      File << str[i];
-    }
     else if (48 <= str[i] && str[i] <= 57) //数字
-    {
       str[i] = ((str[i] + k - 48) % 10) + 48;
-      File << str[i];
-    }
-    else
-      File << str[i];
   }
-  File << endl;
-  File.close();
+  return str;
 }
-string Decode(string &ID, int k = 5) //解密
+string Decode(string &str, int k = 5) //解密
 {
-  ifstream File("Account.dat",
-                std::ios::in);
-  string str;
-  if (File.is_open())
-  {
-    while (true)
-    {
-      if (!(getline(File, str))) //未到末尾
-        break;
-      if (str == ID)
-      {
-        getline(File, str);
-        break;
-      }
-    }
-  }
-  File.close();
+//  ifstream File("Account.dat",
+//                std::ios::in);
+//  string str;
+//  if (File.is_open())
+//  {
+//    while (true)
+//    {
+//      if (!(getline(File, str))) //未到末尾
+//        break;
+//      if (str == ID)
+//      {
+//        getline(File, str);
+//        break;
+//      }
+//    }
+//  }
+//  File.close();
   // cout << str;
-  string ans;
   for (unsigned int i = 0; i < str.length(); i++)
   {
     if (97 <= str[i] && str[i] <= 122) //小写
-    {
       str[i] = ((str[i] - k - 122) % 26) + 122;
-      ans += str[i];
-    }
     else if (65 <= str[i] && str[i] <= 90) //大写
-    {
       str[i] = ((str[i] - k - 90) % 26) + 90;
-      ans += str[i];
-    }
     else if (48 <= str[i] && str[i] <= 57) //数字
-    {
       str[i] = ((str[i] - k - 57) % 10) + 57;
-      ans += str[i];
-    }
-    else
-      ans += str[i];
   }
-  return ans;
+  return str;
 }
 
 #endif // CRASA_H
